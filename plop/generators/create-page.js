4563c3ca4313cc5601f071f24316acac2688e553
quanna19596 <quanna.195.96@gmail.com>
2023-04-14 11:34:40 +0700

    fix create page plop

--- plop/generators/create-page.js
+++ plop/generators/create-page.js
@@ -142,8 +142,9 @@ export default (plop) => ({
       {
         type: PLOP_ACTION_TYPE.MODIFY,
         path: `${PATH.SRC.ROUTER}/utils/lazy-importter.ts`,
-        pattern: new RegExp('/(// [END] ' + capitalize(correctPageType) + ')/', 'g'),
-        template: "export const {{constantCase pageName}} = lazy(() => retryLoadComponent(() => import('@/pages/{{correctPageType}}/{{constantCase pageName}}')));" + BREAK_LINE + '$1',
+        pattern: new RegExp('(// \\[END\\] ' + capitalize(correctPageType) + ')', 'g'),
+        template: "export const {{pascalCase pageName}} = lazy(() => retryLoadComponent(() => import('@/pages/{{correctPageType}}/{{pascalCase pageName}}')));" + BREAK_LINE + '$1',
+        data: { correctPageType }
       },
       { type: PLOP_ACTION_TYPE.PRETTIER }
     ];
