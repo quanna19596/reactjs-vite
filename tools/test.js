b5abc5e3357e5d365cc267a27bf6ee20ceea460e
quanna19596 <quanna.195.96@gmail.com>
2023-04-14 17:03:28 +0700

    add full colors

--- tools/test.js
+++ tools/test.js
@@ -1,15 +1,9 @@
-import { readFile } from './utils.js';
+// @ts-nocheck
+const getColors = () => {
+  const selectColorEl = document.getElementById('colorop');
+  const [defaultOption, ...colorOptions] = selectColorEl?.getElementsByTagName('option');
+  const colorsInScss = colorOptions.map(({ label, value }) => `$${label.replace(/ /g, '-').toLowerCase()}: #${value.toLowerCase()};`).join('');
+  const colorsInEnum = colorOptions.map(({ label, value }) => `${label.replace(/ /g, '_').toUpperCase()} = '#${value.toLowerCase()}'`).join(',');
 
-const fileData = readFile('./src/router/config.ts');
-
-const BREAK_LINE = process.platform.startsWith('win') ? '\r\n' : '\n';
-
-const rg = new RegExp(`{([${BREAK_LINE}].*?)(.*?(?:path: EPagePath.${'SIGN_IN'}).*)(([${BREAK_LINE}]+([^${BREAK_LINE}]+)){7})`, 'g');
-
-const layouts = ['DashboardLayout', 'LandingLayout'];
-
-const pageObj = fileData.match(rg)[0];
-
-const layout = layouts.find((l) => pageObj.includes(l));
-
-console.log(layout);
+  console.log(colorsInScss);
+};
